32e5630b84aa7e95891c3a3bc635b5b54fbc2860
mishraankush105-tech <mishraankush105@gmail.com>
2026-02-16 19:22:27 +0530

    almost compleate basic of maths

--- src/maths/digit/Divisor.java
+++ src/maths/digit/Divisor.java
@@ -4,8 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Divisor {
-    public static void main(String[] args) {
-        int n = 26;
-        int sum = 0;
-
+//    public static int allDivisor(int n){
+//      ArrayList result = new ArrayList<>();
+//        for(int i=1; i<=n;i++){
+//            if(n%i == 0){
+//                result.add(i);
+//            }
+//        }
+//        return result;
+//    }
+//    public static void main(String[] args) {
+//        int n = 26;
+//
+//        ArrayList<Integer> list = new ArrayList();
+//
+//
+//
+//        for(int nums: result){
+//            System.out.print();
+//        }
+//    }
 }
